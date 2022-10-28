52fedcfe12dbf9cd9acc0ded5bc5c11ee4a07eea
CentipedeSully <Centipedesully@gmail.com>
2022-10-28 15:40:16 -0500

    Shoot At Target Implemented
    > Enemies shoot at the player when in range
    > Debugging Gizmo available also

---
+++ Assets/Scripts/Enemy Behavior/EnemyBrain.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBrain : MonoBehaviour
+{
+
+
+    //Attack Target if inRange
+    //Approach Target if not in range
+
+
+
+
+
+}
